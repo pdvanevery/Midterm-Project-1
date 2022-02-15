5d78bc615091ad615d20ee6cb5022e1f9f4895c1
Parker Van Every <parkervanevery@Parkers-MacBook-Pro.local>
2022-02-14 20:52:53 -0800

    updated the attacking system, so far attacks 1 and 2 sort of work, counter and attack 3 need to be implemented properly

--- main-game-files/game.js
+++ main-game-files/game.js
@@ -1,18 +1,19 @@
 //move(player('assets/buggy-filler.jpeg')).to(200, 450)//
 
 //Global Vairables
-const yourMove;
-const oppMove;
-const savedOppMove;
-const yourHealth = 100;
-const oppHealth = 100;
+let yourMove;
+let oppMove;
+let savedOppMove;
+let yourHealth = 100;
+let oppHealth = 100;
 
 //turn counters
-const totRounds = 0;
+let totRounds = 0;
 
 
 //Variables for moves
-const result;
+let result;
+const playByPlay = document.getElementById("announcements");
 const yourHealthBar = document.getElementById("yourHealthBar");
 const oppHealthBar = document.getElementById("oppHealthBar");
 const attackButtonL = document.getElementById("attack1");
@@ -20,6 +21,11 @@ const attackButtonM = document.getElementById("attack2");
 const attackButtonH = document.getElementById("attack3");
 const counterButton = document.getElementById("counter");
 const playAgain = document.getElementById("playAgain");
+
+attackButtonL.addEventListener("click", fight);
+attackButtonM.addEventListener("click", fight);
+attackButtonH.addEventListener("click", fight);
+counterButton.addEventListener("click", fight);
 
 //run on load bc will disable during opp turn
 function enableButtons() {
@@ -30,9 +36,9 @@ function enableButtons() {
 }
 
 //starts the fight
-function fight(id) {
+function fight(e) {
     addRound();
-    oppMove(id);
+    oppMoveA(e.target.id);
     healthChange();
     gameOver();
 }
@@ -45,50 +51,75 @@ function addRound() {
 //adds counter action to the attacks
 //counter to light attack
 function counter(y, o) {
-    const move = Math.floor((Math.random()*6));
-    if (move >=6 && y === "attack1") {
-        result = "Opponent's counter was successful! You took 10 damage";
-        yourHealth -= 10;
-    } else if (move >= 6 && y === "counter") {
-        result = "Your counter was succcessful! Opponent took 10 damage";
-        oppHealth -= 10;
-    } else if (move < 6 && y === "attack1") {
-        result = "Opponent's counter failed! You dealt 12 damage!";
-        oppHealth -= 12;
-    } else if (move < 6 && y === "counter") {
-        result = "Your counter was not successful! your were dealth 12 damage!";
-        yourHealth -= 12;
+    let move = Math.floor((Math.random()*6)+1);
+    if (y === "attack1") {
+        if (move >=6){
+            result = "Opponent's counter was successful! You took 10 damage";
+            yourHealth -= 10;
+            console.log("line 58", yourHealth);
+        } 
+        else {
+            result = "Opponent's counter failed! You dealt 12 damage!";
+            oppHealth -= 12;
+            console.log("line 63", oppHealth);
+        } 
+    } 
+     else if (y === "attack2") {
+        if (move >=5){
+            result = "Opponent's counter was successful! You took 15 damage";
+            yourHealth -= 15;
+            console.log("line 70", yourHealth);
+        } 
+        else {
+            result = "Opponent's counter failed! You dealt 17 damage!";
+            oppHealth -= 17;
+            console.log("line 75", oppHealth);
+        }
+    }
+   else if (y === "attack3") {
+        if (move >=4){
+            result = "Opponent's counter was successful! You took 22 damage";
+            yourHealth -= 22;
+            console.log("line 82", yourHealth);
+        }
+        else {
+            result = "Opponent's counter failed! You dealt 25 damage!";
+            oppHealth -= 25;
+            console.log("line 87", oppHealth);
+        }
     }
 
-    const move = Math.floor((Math.random()*6));
-    if (move >=5 && y === "attack2") {
-        result = "Opponent's counter was successful! You took 15 damage";
-        yourHealth -= 15;
-    } else if (move >= 5 && y === "counter") {
-        result = "Your counter was succcessful! Opponent took 15 damage";
-        oppHealth -= 15;
-    } else if (move < 5 && y === "attack2") {
-        result = "Opponent's counter failed! You dealt 17 damage!";
-        oppHealth -= 17;
-    } else if (move < 5 && y === "counter") {
-        result = "Your counter was not successful! your were dealth 17 damage!";
-        yourHealth -= 17;
-    }     
+    // if (move >= 6 && y === "counter") {
+    //     result = "Your counter was succcessful! Opponent took 10 damage";
+    //     oppHealth -= 10;
+    //     console.log("line 60", oppHealth);
+    // } else {
+    //     result = "Your counter was not successful! your were dealth 12 damage!";
+    //     yourHealth -= 12;
+    //     console.log("line 68", yourHealth);
+    // }
+
+       
+    // if (move2 >= 5 && y === "counter") {
+    //     result = "Your counter was succcessful! Opponent took 15 damage";
+    //     oppHealth -= 15;
+    //     console.log("line 77", oppHealth);
+    // } else {
+    //     result = "Your counter was not successful! your were dealth 17 damage!";
+    //     yourHealth -= 17;
+    //     console.log("line 84", yourHealth);
+    // }     
     
-    const move = Math.floor((Math.random()*6));
-    if (move >=4 && y === "attack3") {
-        result = "Opponent's counter was successful! You took 15 damage";
-        yourHealth -= 20;
-    } else if (move >= 4 && y === "counter") {
-        result = "Your counter was succcessful! Opponent took 15 damage";
-        oppHealth -= 20;
-    } else if (move < 4 && y === "attack3") {
-        result = "Opponent's counter failed! You dealt 25 damage!";
-        oppHealth -= 25;
-    } else if (move < 4 && y === "counter") {
-        result = "Your counter was not successful! your were dealth 25 damage!";
-        yourHealth -= 25;
-    }     
+   
+    // if (move3 >= 4 && y === "counter") {
+    //     result = "Your counter was succcessful! Opponent took 15 damage";
+    //     oppHealth -= 20;
+    //     console.log("line 94", oppHealth);
+    // } else {
+    //     result = "Your counter was not successful! your were dealt 25 damage!";
+    //     yourHealth -= 25;
+    //     console.log("line 101", yourHealth);
+    //}     
 }
 
 //Display results for the round and health bar
@@ -117,21 +148,23 @@ function gameOver() {
 //Game mechanics
 
 //Generates a move for the opponent to use after the player chooses a move
-function oppMove(id) {
-    const move = 
-    Math.floor((Math.random()*5)+1);
-    if (move <= 4) {
+function oppMoveA(id) {
+    let moveC = 
+    Math.floor((Math.random()*6)+1);
+    if (moveC <= 4) {
         savedOppMove = "attack1";
-    } else if (move = 3)  {
+    } else if (moveC = 3)  {
         savedOppMove = "attack2";
-    } else if (move = 2) {
+    } else if (moveC = 2) {
         savedOppMove = "attack3";
     } else {
         savedOppMove = "counter";
     };
 
     result = ("Your move is <span>" + id + "</span> and the computers move is <span>" + savedOppMove + "</span> on round " + totRounds);
+    console.log(id, savedOppMove);
     damageStep(id, savedOppMove);
+    damageStep2(id, savedOppMove);
     roundResults(result);
 
 }
@@ -144,19 +177,44 @@ function damageStep(y, o) {
         if (oppHealth >=10 && yourHealth >= 10) {
             oppHealth -= 10;
             yourHealth -= 10;
+            console.log("line 178", oppHealth);
+            console.log("line 179", yourHealth);
         } else {
             oppHealth = 0;
             yourHealth = 0
         }
     } else if (y === "counter" && o === "counter") {
         result = "Both players attempted to counter, counters failed";
-    } else if ( y === "attack1" && o === "counter") {
+    } else if ( y === "attack1" && o === "attack2") {
         result = "Opponent is ready to counter";
         counter(y, o);
-    } else if (y === "counter" && c === "attack1") {
+    } else if (y === "counter" && o === "attack1") {
         result = "You are ready to counter";
         counter(y, o );
     }
+}
+
+function damageStep2(y, o) {
+    if (y === "attack2" && o === "attack2") {
+        result = "Both players took damage";
+        if (oppHealth >=15 && yourHealth >= 15) {
+            oppHealth -= 15;
+            yourHealth -= 15;
+            console.log("line 201", oppHealth);
+            console.log("line 202", yourHealth);
+        } else {
+            oppHealth = 0;
+            yourHealth = 0
+        }
+    } else if (y === "counter" && o === "counter") {
+        result = "Both players attempted to counter, counters failed";
+    } else if ( y === "attack2" && o === "attack1") {
+        result = "Opponent is ready to counter";
+        counter(y, o);
+    } else if (y === "counter" && o === "attack2") {
+        result = "You are ready to counter";
+        counter(y, o );
+    } 
 }
 
 window.onload=enableButtons();
